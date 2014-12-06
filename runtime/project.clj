9a6ea7ca4cb7afc504fd6d8fa42e29c96853564b
James Henderson <james@jarohen.me.uk>
2014-12-06 23:58:47 +0000

    Making the version number common to plugin + runtime

--- runtime/project.clj
+++ runtime/project.clj
@@ -1,7 +1,11 @@
-(defproject jarohen/phoenix.runtime "0.1.0-SNAPSHOT"
+(def version (slurp "../common/phoenix-version"))
+
+(defproject jarohen/phoenix.runtime version
   :description "The runtime for the Phoenix plugin"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  :resource-paths ["resources" "../common"])
