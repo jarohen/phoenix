940e7c267044b5066fed53f86ccf26b7ac02a3ac
James Henderson <james@jarohen.me.uk>
2014-12-07 15:12:18 +0000

    Adding deps to runtime

--- runtime/project.clj
+++ runtime/project.clj
@@ -6,6 +6,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.namespace "0.2.7"]
+                 [org.clojure/tools.nrepl "0.2.6"]                 
+                 [com.stuartsierra/component "0.2.2"]
+                 [com.stuartsierra/dependency "0.1.1"]
+                 [medley "0.5.3"]]
+  
   :resource-paths ["resources" "../common"])
