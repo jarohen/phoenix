c35b54ff2fb7a9bce900d772ff96bd9fe8e1452c
James Henderson <james@jarohen.me.uk>
2014-12-10 23:15:15 +0000

    Keeping access to the system even when it's not started

--- runtime/src/phoenix.clj
+++ runtime/src/phoenix.clj
@@ -11,37 +11,48 @@
 
 (def ^:private config-resource)
 
-(defonce !system
-  (atom nil))
+(def system nil)
+
+(defonce ^:private !started?
+  (atom false))
 
 (defonce ^:private !location
   (let [location (l/get-location)]
     (atom {:original location
            :current location})))
 
+(defn- make-system [location]
+  (-> (config/read-config config-resource
+                          {:location location})
+      phoenix-system))
+
 (defn set-location! [{:keys [environment host] :as new-location}]
-  (assert (nil? @!system) "Can't change location when system is running...")
+  (assert (false? @!started?) "Can't change location when system is running...")
 
   (let [new-location (swap! !location
                             (fn [{:keys [original] :as old-location}]
                               (assoc old-location
                                 :current (merge original new-location))))]
+
     (println "Setting Phoenix location:" (pr-str (:current new-location)))
+    
+    (alter-var-root #'system (constantly (make-system new-location)))
     new-location))
 
 (defn- do-start! []
-  (reset! !system (-> (config/read-config config-resource
-                                          {:location (:current @!location)})
-                      phoenix-system
-                      c/start-system)))
+  (let [started-system (alter-var-root #'system
+                                       (comp c/start-system
+                                             (constantly (make-system (:current @!location)))))]
+    (reset! !started? true)
+    started-system))
 
 (defn start! []
-  (assert (nil? @!system) "System already started!")
+  (assert (false? @!started?) "System already started!")
   (tn/refresh :after 'phoenix/do-start!))
 
 (defn stop! []
-  (c/stop-system @!system)
-  (reset! !system nil))
+  (alter-var-root #'system c/stop-system)
+  (reset! !started? false))
 
 (defn reload! [& [{:keys [environment host] :as new-location}]]
   (stop!)
@@ -55,6 +66,7 @@
           "Please make sure you have a valid config resource specified at ':phoenix/config' in your 'project.clj'")
   
   (alter-var-root #'config-resource (constantly (io/resource phoenix-config)))
+  (alter-var-root #'system (constantly (make-system @!location)))
 
   (when-let [nrepl-port (:phoenix/nrepl-port (read-string (slurp (io/resource phoenix-config))))]
     (start-nrepl! nrepl-port project))
