0d66cf1f81e173d44dd575b54d7d5f47adb5207e
James Henderson <james@jarohen.me.uk>
2015-01-10 18:45:01 +0000

    http-kit should probably have some more deps

--- modules/http-kit/project.clj
+++ modules/http-kit/project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [com.stuartsierra/component "0.2.2"]
                  [http-kit "2.1.18"]
                  
                  [juxt.modular/ring "0.5.2"]])
