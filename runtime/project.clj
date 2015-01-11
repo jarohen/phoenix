c65e44281942268e1d655f17a462169ccd4fd4b3
James Henderson <james@jarohen.me.uk>
2015-01-11 10:44:22 +0000

    [runtime] should explicitly depend on tools.logging

--- runtime/project.clj
+++ runtime/project.clj
@@ -10,7 +10,9 @@
   
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.namespace "0.2.7"]
-                 [org.clojure/tools.nrepl "0.2.6"]                 
+                 [org.clojure/tools.nrepl "0.2.6"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 
                  [com.stuartsierra/component "0.2.2"]
                  [com.stuartsierra/dependency "0.1.1"]
                  [medley "0.5.3"]]
