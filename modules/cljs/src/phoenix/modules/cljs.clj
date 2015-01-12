be61bfa6178bddfe1a35329566fad9044e984b23
James Henderson <james@jarohen.me.uk>
2015-01-12 13:21:53 +0000

    [cljs] using the caching now provided by shadow-build

--- modules/cljs/src/phoenix/modules/cljs.clj
+++ modules/cljs/src/phoenix/modules/cljs.clj
@@ -26,6 +26,7 @@
 
                          (log-time-end [_ msg ms]
                            (log/log 'shadow.cljs.build :info nil (format "<- %s (%dms)" msg ms)))))
+        
         (cljs/step-find-resources-in-jars))))
 
 (defprotocol CLJSComponent
@@ -49,7 +50,9 @@
       (assoc :optimizations optimizations
              :pretty-print pretty-print?
              :public-path web-context-path
-             :public-dir output-dir
+             :public-dir (io/file output-dir "public")
+             :cache-dir (io/file output-dir "cache")
+             :work-dir (io/file output-dir "work")
              :externs externs)
       
       (cljs/step-find-resources source-path)
@@ -110,7 +113,7 @@
 
   CLJSComponent
   (bidi-routes [{:keys [web-context-path output-dir]}]
-    [web-context-path (bs/files {:dir output-dir})])
+    [web-context-path (bs/files {:dir (.getPath (io/file output-dir "public"))})])
 
   (cljs-handler [this]
     (br/make-handler (bidi-routes this)))
@@ -144,36 +147,4 @@
   (if built?
     (map->PreBuiltCLJSComponent (combine-opts opts :build))
     (map->CLJSCompiler (combine-opts opts :dev))))
-
-(comment
-  (def foo-state
-    (-> (cljs/init-state)
-        (assoc :optimizations :none
-               :pretty-print true
-               :work-dir (io/file "target/cljs-work")
-               :cache-dir (io/file "target/cljs-cache")
-               :public-dir (io/file "target/resources/js")
-               :public-path "/js"
-               :logger (reify cljs/BuildLog
-                         (log-warning [_ msg]
-                           (log/log 'shadow.cljs.build :warn nil msg))
-
-                         (log-progress [_ msg]
-                           (log/log 'shadow.cljs.build :debug nil msg))
-
-                         (log-time-start [_ msg]
-                           (log/log 'shadow.cljs.build :info nil (format "-> %s" msg)))
-
-                         (log-time-end [_ msg ms]
-                           (log/log 'shadow.cljs.build :info nil (format "<- %s (%dms)" msg ms)))))
-      
-        (cljs/step-find-resources-in-jars)
-        (cljs/step-find-resources "ui-src")
-
-        (cljs/step-finalize-config)
-        (cljs/step-compile-core)
-        (cljs/step-configure-module :main ['test-project.ui.app] #{})
-      
-        (cljs/step-compile-modules)
-        (cljs/flush-unoptimized))))
 
