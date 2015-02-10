35e8ce0ae4d78674ee7c497ce342e266891bcaac
James Henderson <james@jarohen.me.uk>
2015-02-10 09:28:17 +0000

    Adding a simple JDBC pool module

---
+++ modules/jdbc-pool/project.clj
@@ -0,0 +1,11 @@
+(defproject jarohen/phoenix.modules.jdbc-pool "0.0.3"
+  :description "A module to set up a JDBC connection pool component in Phoenix"
+  :url "https://github.com/james-henderson/phoenix"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [com.stuartsierra/component "0.2.2"]
+
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.apache.commons/commons-dbcp2 "2.0.1"]])
---
+++ modules/jdbc-pool/src/phoenix/modules/jdbc_pool.clj
@@ -0,0 +1,41 @@
+(ns phoenix.modules.jdbc-pool
+  (:require [com.stuartsierra.component :as c]
+            [clojure.tools.logging :as log])
+  (:import [org.apache.commons.dbcp2 BasicDataSource]))
+
+(defprotocol DatabasePool
+  (db-conn [db]))
+
+(def known-drivers
+  {"postgresql" "org.postgresql.Driver"
+   "mysql" "com.mysql.jdbc.Driver"
+   "mssql" "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+   "odbc" "sun.jdbc.odbc.JdbcOdbcDriver"
+   "sqlite" "org.sqlite.JDBC"
+   "h2" "org.h2.Driver"})
+
+(defrecord DatabaseComponent []
+  c/Lifecycle
+  (start [{:keys [driver subprotocol host port username password db] :as this}]
+    (log/info "Starting JDBC pool...")
+
+    (assoc this
+      ::pool (doto (BasicDataSource.)
+               (.setDriverClassName (or driver (get known-drivers subprotocol)))
+               (.setAccessToUnderlyingConnectionAllowed true)
+               (.setUrl (format "jdbc:%s://%s:%s/%s" subprotocol host port db))
+               (.setUsername username)
+               (.setPassword password))))
+  
+  (stop [{:keys [::pool] :as this}]
+    (when pool
+      (.close pool))
+    
+    (dissoc this ::pool))
+
+  DatabasePool
+  (db-conn [{:keys [::pool]}]
+    {:datasource pool}))
+
+(defn make-jdbc-pool [opts]
+  (map->DatabaseComponent opts))
