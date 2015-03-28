1e04e9829649ed37d5a4e41517b3a90de44c5d10
James Henderson <james@jarohen.me.uk>
2015-03-28 11:59:49 +0000

    Moving resolving references out to the loader instead - and walking the structure s.t. references can be dereferenced at any level of nesting

--- runtime/src/phoenix/analyzer.clj
+++ runtime/src/phoenix/analyzer.clj
@@ -8,13 +8,7 @@
 (defmulti analyze-config-pair
   (fn [config acc k v]
     (or (#{:phoenix/component} k)
-        (#{:phoenix/dep} v)
-
-        (and (vector? v)
-             (let [[fst & more] v]
-               (when (and (keyword? fst)
-                          (= (namespace fst) "phoenix"))
-                 fst))))))
+        (#{:phoenix/dep} v))))
 
 (defmethod analyze-config-pair :phoenix/dep [_ acc k v]
   (let [referred-component (or (when (= v :phoenix/dep)
@@ -26,30 +20,6 @@
 
 (defmethod analyze-config-pair :phoenix/component [_ acc _ v]
   (assoc acc :component-fn v))
-
-(defn read-env-var [var-name]
-  (System/getenv (csk/->SNAKE_CASE_STRING var-name)))
-
-(defmethod analyze-config-pair :phoenix/env-var [_ acc k [_ var-name default]]
-  (assoc-in acc [:component-config k] (or (read-env-var var-name) default)))
-
-(defmethod analyze-config-pair :phoenix/edn-env-var [_ acc k [_ var-name default]]
-  (let [env-value (read-env-var var-name)
-        edn-env-value (try
-                        (when env-value
-                          (edn/read-string env-value))
-                        (catch Exception e
-                          (throw (ex-info "Phoenix: failed reading env-var"
-                                          {:env-var var-name
-                                           :value env-value}))))]
-
-    (assoc-in acc [:component-config k] (or edn-env-value default))))
-
-(defmethod analyze-config-pair :phoenix/secret [{:keys [:phoenix/secret-keys]} acc k [_ secret-key-name cypher-text]]
-  (let [secret-key (get secret-keys secret-key-name)]
-    (assert secret-key (format "Phoenix: can't find secret key '%s'" secret-key-name))
-
-    (assoc-in acc [:component-config k] (ps/decrypt cypher-text (get secret-keys secret-key-name)))))
 
 (defmethod analyze-config-pair :default [_ acc k v]
   (assoc-in acc [:component-config k] v))
