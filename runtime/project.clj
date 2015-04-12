905b9c3102c40e676cf98d04b5522dc4c7373ffb
James Henderson <james@jarohen.me.uk>
2015-04-12 11:25:00 +0100

    [runtime] Bumping versions of all the things

--- runtime/project.clj
+++ runtime/project.clj
@@ -9,20 +9,20 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.namespace "0.2.7"]
-                 [org.clojure/tools.nrepl "0.2.6"]
+                 [org.clojure/tools.namespace "0.2.10"]
+                 [org.clojure/tools.nrepl "0.2.10"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/tools.reader "0.8.13"]
+                 [org.clojure/tools.reader "0.9.1"]
 
                  [jarohen/phoenix.build "0.0.1"]
 
                  [camel-snake-kebab "0.3.1" :exclusions [org.clojure/clojure]]
 
-                 [com.stuartsierra/component "0.2.2"]
+                 [com.stuartsierra/component "0.2.3"]
                  [com.stuartsierra/dependency "0.1.1"]
-                 [medley "0.5.3"]
+                 [medley "0.5.5"]
 
-                 [buddy/buddy-core "0.3.0"]
+                 [buddy/buddy-core "0.5.0"]
 
                  [prismatic/schema "0.4.0"]]
 
