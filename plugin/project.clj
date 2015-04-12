18205dae5878aeb362588e55680f3a6bfd75d415
James Henderson <james@jarohen.me.uk>
2015-04-12 12:00:02 +0100

    [plugin] updating leinjacker

--- plugin/project.clj
+++ plugin/project.clj
@@ -8,7 +8,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[leinjacker "0.4.1"]]
+  :dependencies [[leinjacker "0.4.2"]]
 
   :resource-paths ["resources" "../common"]
   
