547bea8c427904a3a2e36508f67c2887cb74d355
James Henderson <james@jarohen.me.uk>
2016-03-07 08:50:12 +0000

    Bumping versions - 0.1.3

--- runtime/project.clj
+++ runtime/project.clj
@@ -8,7 +8,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [org.clojure/tools.nrepl "0.2.10"]
                  [org.clojure/tools.logging "0.3.1"]
